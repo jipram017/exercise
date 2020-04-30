bd89b7043a8a9295615c2e817c14b058a50928f6
jipram019 <jipram019@gmail.com>
2020-04-30 11:11:37 +0700

    jumpgameIII

--- src/exercise/JumpGame.java
+++ src/exercise/JumpGame.java
@@ -36,10 +36,30 @@ public class JumpGame {
 		}
 		return jumpable;
 	}
+	
+	//Time complexity: O(n)
+	//Space complexity: O(n)
+	public static int minimumJump(int[] nums) {
+		if(nums.length <= 1) return 0;
+		
+		int maxReach = nums[0], steps = nums[0], jump = 0;
+		for(int i = 0; i < nums.length; i++) {
+			if(i == nums.length-1) break;
+			maxReach = Math.max(maxReach, i+nums[i]);
+			steps--;
+			if(steps == 0) {
+				if(i >= maxReach) return -1;
+				jump++;
+				steps = maxReach-i;
+			}
+		}
+		return jump+1;
+	}
+	
 
 	public static void main(String[] args) {
-		int[]array1 = {3,2,1,0,4};
-		System.out.println(canJump2(array1));
+		int[]array1 = {2,3,1,1,4};
+		System.out.println(minimumJump(array1));
 	}
 
 }
---
+++ src/exercise/JumpGameIII.java
@@ -0,0 +1,37 @@
+package exercise;
+
+public class JumpGameIII {
+	public static boolean canReachZeroValue(int[] nums, int start) {
+		if(nums.length == 0 || nums == null || start < 0 || start >= nums.length) {
+			return false;
+		} 
+		return findZeroValue(nums, start, new boolean[nums.length], nums.length);
+	}
+	
+	public static boolean findZeroValue(int[]nums, int start, boolean[] visited, int length) {
+		if(start < 0 || start >= length) {
+			return false;
+		}
+		if(visited[start]) {
+			return false;
+		}
+		if(nums[start] == 0) {
+			return true;
+		} else {
+			visited[start] = true;
+			return findZeroValue(nums, start+nums[start], visited, nums.length) ||
+				   findZeroValue(nums, start-nums[start], visited, nums.length);
+		}	
+	}
+	
+	public static void main(String[] args) {
+		int[] array = new int[] {3,0,2,1,2};
+		System.out.println(canReachZeroValue(array,2));
+		
+		array = new int[] {4,2,3,0,3,1,2};
+		System.out.println(canReachZeroValue(array,5));
+		
+		array = new int[] {4,2,3,0,3,1,2};
+		System.out.println(canReachZeroValue(array,3));
+	}
+}
