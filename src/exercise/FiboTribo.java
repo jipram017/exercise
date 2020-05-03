4c02f2bde9eb0f817c94d104a0a9d15505c051a0
jipram019 <jipram019@gmail.com>
2020-05-03 21:49:23 +0700

    fibotribo

---
+++ src/exercise/FiboTribo.java
@@ -0,0 +1,25 @@
+package exercise;
+
+public class FiboTribo {
+	public int fibonacci(int n) {
+		int t0 = 0, t1 = 1, res = 0;
+		for(int i = 2; i <= n; i++){
+			res = t0+t1; t0 = t1; t1 = res;
+	    }
+	    return n == 0?0:n==1? 1: res;
+	}
+	
+	public int tribonacci(int n) {
+		int t0 = 0, t1 = 1, t2 = 1, res = 0;
+		for(int i = 3; i <= n; i++){
+			res = t0+t1+t2; t0 = t1; t1 = t2; t2 = res;
+	    }
+	    return n == 0?0:(n==1 || n==2)? 1: res;
+	}
+	   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
