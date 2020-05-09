d1acf241abf12a9f3ee36aaa27fbaf9f3547c869
jipram019 <jipram019@gmail.com>
2020-05-10 01:15:03 +0700

    updated

--- src/exercise/MaxSumBST.java
+++ src/exercise/MaxSumBST.java
@@ -2,28 +2,27 @@ package exercise;
 import exercise.FlattenBinaryTree.TreeNode;
 
 public class MaxSumBST {
-	
+	private static int max;
 	public static int maxSumBST(TreeNode root) {
-		int[] max = new int[1];
-		maxSumBST(root, max);
-		return max[0];
+		maxSumBSTHelper(root);
+		return max;
 	}
 	
-	public static int[] maxSumBST(TreeNode root, int[] max) {
+	public static int[] maxSumBSTHelper(TreeNode root) {
 		if (root == null) return null;
-		int[] left = maxSumBST(root.left, max);
-		int[] right = maxSumBST(root.right, max);
+		int[] left = maxSumBSTHelper(root.left);
+		int[] right = maxSumBSTHelper(root.right);
 		if(left != null && (left[1] >= root.val || left[3] == 0)){
-			return new int[]{0, -1, Integer.MAX_VALUE, 0};
+			return new int[]{0, -1, -1, 0};
 		}
 		if(right != null && (right[2] <= root.val || right[3] == 0)){
-			return new int[]{0, Integer.MIN_VALUE, Integer.MAX_VALUE, 0};
+			return new int[]{0, -1, -1, 0};
 		}
 		
 		int leftSum = left == null ? 0 : left[0];
 		int rightSum = right == null ? 0 : right[0];
 		int sum = leftSum + rightSum + root.val;
-		max[0] = Math.max(max[0], sum);
+		max = Math.max(max, sum);
 		
 		int maxVal = right == null ? root.val : right[2];
 		int minVal = left == null ? root.val : left[1];
