a3e859bc369dba2365dea170221f33ed84132002
jipram019 <jipram019@gmail.com>
2020-05-19 02:27:39 +0700

    geometry

---
+++ src/exercise/LineMaxPoints.java
@@ -0,0 +1,58 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Time O(n^2), Space O(1)
+public class LineMaxPoints {
+    public int maxPoints(int[][] points) {
+    	if(points.length == 0 || points == null) return 0;
+    	int finalCount = 0;
+    	Map<Integer, Map<Integer, Integer>> result = new HashMap<>();
+    	
+        for(int i = 0; i < points.length; i++) {
+            result.clear();
+        	int duplicate = 1, maxCount = 0;
+        	for(int j = i+1; j < points.length; j++) {
+    			int dy = points[j][1] - points[i][1];
+    			int dx = points[j][0] - points[i][0];
+    			if(dx == 0 && dy == 0) {
+    				duplicate++;
+    				continue;
+    			}
+    			
+    			int d = generateGCD(dx, dy);
+    			if(d != 0) {
+    				dx /= d; dy /= d;
+    			}
+        			
+        		if(result.containsKey(dx)) {
+        			if(result.get(dx).containsKey(dy)) {
+        				result.get(dx).put(dy, result.get(dx).get(dy) + 1);
+        			} else {
+        				result.get(dx).put(dy, 1);
+        			} 
+        		} else {
+        			Map<Integer, Integer> m = new HashMap<>();
+        			m.put(dy, 1);
+        			result.put(dx, m);
+        		}
+        			
+        		maxCount = Math.max(maxCount, result.get(dx).get(dy));	
+        	}	
+        	finalCount = Math.max(finalCount, maxCount+duplicate);
+        } 
+        return finalCount;
+    }
+    
+    private int generateGCD(int a, int b) {
+    	if(b == 0) return a;
+    	return generateGCD(b, a%b);
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/exercise/RectangleMania.java
@@ -0,0 +1,37 @@
+package exercise;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Time: O(n^2), Space: O(1)
+public class RectangleMania {
+	public static int countRectangle(int[][] coordinates) {
+		if(coordinates == null || coordinates.length == 0) return 0;
+		Set<String> coordSets = new HashSet<>();
+		int rectangleCount = 0;
+		for(int[] coord : coordinates) {
+			String coordString = Integer.toString(coord[0]) + "-" + Integer.toString(coord[1]);
+			coordSets.add(coordString);
+		}
+		
+		for(int[] coord : coordinates) {
+			for(int[] coord2 : coordinates) {
+				if(coord2[0] <= coord[0] || coord2[1] <= coord[1]) {
+					continue;
+				}		
+				String upperLeft = Integer.toString(coord[0]) + "-" + Integer.toString(coord2[1]);
+				String bottomRight = Integer.toString(coord2[0]) + "-" + Integer.toString(coord[1]);
+				if(coordSets.contains(upperLeft) && coordSets.contains(bottomRight)) {
+					rectangleCount += 1;
+				}
+			}
+		}
+		return rectangleCount;
+	}
+	public static void main(String[] args) {
+		int[][] coordinates = new int[][] {{0,0},{1,0},{2,0},{0,1},{1,1},{2,1},{0,2},{1,2}};
+		int count = countRectangle(coordinates);
+		System.out.println(count);
+	}
+
+}
