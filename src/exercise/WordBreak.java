1332c328f5c4a200ebe326ee7e39bcc7987024fc
jipram019 <jipram019@gmail.com>
2020-05-24 02:14:02 +0700

    word break II

--- src/exercise/WordBreak.java
+++ src/exercise/WordBreak.java
@@ -4,55 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 
 public class WordBreak {
-	private static class TrieNode{
-		TrieNode[] next = new TrieNode[26];
-		boolean word;
-	}
-	
 	// Time complexity O(n^2)
-	// Space complexity O(n^2) where n =  size of dict
-	private static TrieNode buildTrie(List<String> dict) {
-		TrieNode root = new TrieNode();
-		for (String word : dict) {
-			TrieNode node = root;
-			for(char c : word.toCharArray()) {
-				int i = c - 'a';
-				if(node.next[i] == null) node.next[i] = new TrieNode();
-				node = node.next[i];
-			}
-			node.word = true;
-		}
-		return root;
-	}
-	
-	private static boolean wordBreak(TrieNode root, String s, boolean[] dp) {
-		dp[0] = true;
-		for(int i = 0; i < s.length(); i++) {
-			if(dp[i]) {
-				int j=i;
-				TrieNode node = root;
-				for(; j < s.length(); j++) {
-					if(node.word) {
-						dp[j] = true;
-					}
-					int c = s.charAt(j) - 'a';
-					node = node.next[c];
-					if(node == null) break;
-				}
-				if(node == null) continue;
-				if(node.word)dp[j] = true; 
-			}
-		}
-		return dp[s.length()];
-	}
-	
+	// Space complexity O(n) where n =  size of wordDict
     public static boolean wordBreak(String s, List<String> wordDict) {
-        if(s == null || s.length() == 0) return true;
-        if(wordDict.isEmpty()) return false;
-        return wordBreak(buildTrie(wordDict), s, new boolean[s.length()+1]);
-    }
-    
-    public static boolean wordBreak2(String s, List<String> wordDict) {
         if(s == null || s.length() == 0) return true;
         if(wordDict.isEmpty()) return false;
         boolean[] pos = new boolean[s.length()+1];
--- src/exercise/WordBreakII.java
+++ src/exercise/WordBreakII.java
@@ -2,50 +2,56 @@ package exercise;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class WordBreakII {
-	
-	public static List<String> wordBreak(String s, List<String> wordDict){
-		Set<String> dict = wordDict.stream().collect(Collectors.toSet());
-		return recursiveWordBreak(s, dict, 0, new HashMap<Integer, List<String>>());
+	// Time complexity O(n^2)
+	// Space complexity O(n) where n =  size of wordDict
+    public static List<String> wordBreak(String s, List<String> wordDict) {
+        if(s == null || s.length() == 0) return new ArrayList<String>();
+        if(wordDict.isEmpty()) return new ArrayList<String>();
+		ArrayList<String>[] pos = new ArrayList[s.length() + 1];
+        pos[0] = new ArrayList<String>();
+        
+        for(int i = 0; i < s.length(); i++) {
+        	if(pos[i] != null) {
+        		for(int j = i+1; j <= s.length(); j++) {
+        			String sub = s.substring(i,j);
+        			if(wordDict.contains(sub)) {
+        				if(pos[j] == null) {
+        					ArrayList<String> list = new ArrayList<String>();
+        					list.add(sub);
+        					pos[j] = list;
+        				} else {
+        					pos[j].add(sub);
+        				}
+        			}
+        		}
+        	}
+        }
+        
+        if(pos[s.length()] == null) {
+        	return new ArrayList<String>();
+        }
+        ArrayList<String> result = new ArrayList<String>();
+        dfs(pos, result, "", s.length());
+        return result;
 	}
-	
-	public static List<String> recursiveWordBreak(String s, Set<String> wordDict, int index, HashMap<Integer, List<String>> map){
-		if(index == s.length()) {
-			return Collections.singletonList("");
-		}
-		
-		if(map.containsKey(index)) {
-			return map.get(index);
-		}
-		
-		List<String> words = new ArrayList<String>();
-		
-		for(int end = index; end <= s.length(); end++) {
-			String sub = s.substring(index, end);
-			if(wordDict.contains(sub)) {
-				List<String> next_sub = recursiveWordBreak(s, wordDict, end, map);
-				for(String str : next_sub) {
-					if(!str.equals("")) {
-						words.add(sub+" "+str);
-					} else {
-						words.add(sub);
-					}
-				}
-			}
-		}
-		
-		map.put(index, words);
-		return words;
-	}
+    
+    public static void dfs(ArrayList<String> [] pos, ArrayList<String> result, String curr, int slen) {
+    	if(slen == 0) {
+    		result.add(curr.trim());
+    		return;
+    	}
+    	
+    	for(String s : pos[slen]) {
+    		String combined = s + " " + curr;
+    		dfs(pos, result, combined, slen-s.length());
+    	}
+    }
 
 	public static void main(String[] args) {
-		String s = "catsanddogr";
+		String s = "catsanddog";
 	    List<String> wordDict = new ArrayList<String>(Arrays.asList("cat", "cats", "and", "sand", "dog"));
 	    List<String> result = wordBreak(s, wordDict);
 	    for(String str : result) {
