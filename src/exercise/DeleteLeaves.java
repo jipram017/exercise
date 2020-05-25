4f50cff2577ba4ff4fc49e81cd039715a065e6ae
jipram019 <jipram019@gmail.com>
2020-05-25 14:29:20 +0700

    delete leaves BT

---
+++ src/exercise/DeleteLeaves.java
@@ -0,0 +1,21 @@
+package exercise;
+
+import exercise.FlattenBinaryTree.TreeNode;
+
+public class DeleteLeaves {
+	public static TreeNode removeLeafNodes(TreeNode root, int target) {
+        if(root == null) return null;
+		root.left = removeLeafNodes(root.left, target);
+		root.right = removeLeafNodes(root.right, target);
+        if(root.left == null && root.right == null && root.val == target) {
+			return null;
+		}
+		return root;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
