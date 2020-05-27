ab0ae451cee8f0f6db1b1d702e692a708e8be573
jipram019 <jipram019@gmail.com>
2020-05-28 04:21:27 +0700

    merge sorted lists

---
+++ src/exercise/MergeKSortedLists.java
@@ -0,0 +1,41 @@
+package exercise;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import exercise.LinkedListCycle.ListNode;
+
+public class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+        	public int compare(ListNode l1, ListNode l2) {
+        		return l1.val - l2.val;
+        	}
+        });
+        
+        for(ListNode l : lists) {
+        	if(l != null) queue.offer(l);
+        }
+        
+        ListNode p = new ListNode(-1);
+        ListNode head  = p;
+        
+        while(!queue.isEmpty()) {
+        	ListNode top = queue.poll();
+        	head.next = top;
+        	head = top;
+        	if(top.next != null) {
+        		queue.offer(top.next);
+        	}
+        }
+        
+        return p.next;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/exercise/MergeTwoSortedLists.java
@@ -0,0 +1,39 @@
+package exercise;
+
+import exercise.LinkedListCycle.ListNode;
+
+public class MergeTwoSortedLists {
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null) return l2;
+        if(l2 == null) return l1;
+    	ListNode result = new ListNode(-1);
+    	ListNode last = result;
+    	
+        while(l1 != null && l2 != null) {
+        	if(l1.val < l2.val) {
+        		last.next = l1;
+        		last = l1;
+        		l1 = l1.next;
+        	} else {
+        		last.next = l2;
+        		last = l2;
+        		l2 = l2.next;
+        	}
+        }
+        
+        if(l1 != null) {
+        	last.next = l1;
+        }
+        if(l2 != null) {
+        	last.next = l2;
+        }
+        
+        return result.next;  
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
