f86c9a2d72c167419b2ceab00659dc9b8d96590b
jipram019 <jipram019@gmail.com>
2020-05-29 08:31:16 +0700

    valid square

---
+++ src/exercise/ValidSquare.java
@@ -0,0 +1,35 @@
+package exercise;
+
+public class ValidSquare {
+    public static boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
+        long[] sides = new long[] {side(p1,p2), side(p1,p3), side(p1,p4), side(p2,p3), side(p3,p4), side(p2,p4)};
+        long max  = 0, count = 0, noMax = 0;
+        for(long side : sides) {
+        	max = Math.max(max, side);
+        }
+        for(long side : sides) {
+        	if(side == max) {
+        		count++;
+        	} else {
+        		noMax = side;
+        	}
+        }
+        
+        if(count != 2) return false;
+        for(long side : sides) {
+        	if(side != max && side != noMax) return false;
+        }
+        return true;
+    }
+    
+    public static long side(int[] point1, int[] point2) {
+    	double sum = Math.pow(point2[0] - point1[0], 2) + Math.pow(point2[1] - point1[1],2);
+    	return (long) sum;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
