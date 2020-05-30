0772f75412c8c5bfefabaf47d5fb8818b340b27a
jipram019 <jipram019@gmail.com>
2020-05-31 03:26:49 +0700

    largest rect histogram

---
+++ src/exercise/LargestRectangleInHistogram.java
@@ -0,0 +1,38 @@
+package exercise;
+
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+    public static int largestRectangleArea(int[] heights) {
+        if(heights.length == 0) return 0;
+        Stack<Integer> stack = new Stack<>();
+        int i = 0, maxRectangle = 0;
+        
+        while(i < heights.length) {
+        	if(stack.isEmpty() || heights[i] >= heights[stack.peek()]) {
+        		stack.push(i);
+        		i++;
+        	} else {
+        		Integer prev = stack.pop();
+        		int height = heights[prev];
+        		int width = stack.isEmpty()? i : i - stack.peek() - 1;
+        		maxRectangle = Math.max(maxRectangle, height*width);
+        	}
+        }
+        
+        while(!stack.isEmpty()) {
+       		Integer prev = stack.pop();
+    		int height = heights[prev];
+    		int width = stack.isEmpty()? i : i - stack.peek() - 1;
+    		maxRectangle = Math.max(maxRectangle, height*width);
+        }
+        
+        return maxRectangle;  
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
