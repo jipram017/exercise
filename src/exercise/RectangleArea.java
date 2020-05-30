6154ac168fae1031d250140c464b57b76dae9bb5
jipram019 <jipram019@gmail.com>
2020-05-31 00:18:04 +0700

    rectangle area

---
+++ src/exercise/RectangleArea.java
@@ -0,0 +1,25 @@
+package exercise;
+
+public class RectangleArea {
+    public static int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    	if(G < A || C < E) {
+    		return (C-A)*(D-B) + (G-E)*(H-F);
+    	}
+    	if(H < B || D < F) {
+    		return (C-A)*(D-B) + (G-E)*(H-F); 
+    	}
+    	
+    	int right = Math.min(C, G);
+    	int left = Math.max(A, E);
+    	int top = Math.min(D, H);
+    	int bottom = Math.max(B, F);
+    	
+    	return (C-A)*(D-B) + (G-E)*(H-F) - (right-left)*(top-bottom);
+	}
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
