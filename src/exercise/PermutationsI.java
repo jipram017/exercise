0b2bfd513981cc8c6b0818de0c923585bd34ba35
jipram019 <jipram019@gmail.com>
2020-06-01 04:40:33 +0700

    permutations

---
+++ src/exercise/PermutationsI.java
@@ -0,0 +1,45 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationsI {
+	
+	// Time complexity O(n!*n)
+	// Space complexity O(n!*n)
+    public static List<List<Integer>> permute(int[] nums) {
+        if(nums == null || nums.length == 0) return new ArrayList<List<Integer>>();
+        List<List<Integer>> permutations = new ArrayList<List<Integer>>();
+        permutationsHelper(0, nums, permutations);
+        return permutations;
+    }
+    
+    private static void permutationsHelper(int index, int[] nums, List<List<Integer>> permutations){
+    	if(index == nums.length-1) {
+    		List<Integer> l = new ArrayList<Integer>();
+    		for(Integer num : nums) {
+    			l.add(num);
+    		}
+    		permutations.add(l);
+    		return;
+    	}
+    	
+    	for(int i=index; i<nums.length; i++) {
+    		swap(nums, index, i);
+    		permutationsHelper(index+1, nums, permutations);
+    		swap(nums, index, i);
+    	}
+    }
+    
+    private static void swap(int[] nums, int a, int b) {
+    	int temp = nums[a];
+    	nums[a] = nums[b];
+    	nums[b] = temp;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
