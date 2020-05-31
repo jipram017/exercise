f36ce38219528e1aa158a8dd2b9f728e0a1c0977
jipram019 <jipram019@gmail.com>
2020-05-31 18:07:13 +0700

    check overlap

---
+++ src/exercise/CheckOverlap.java
@@ -0,0 +1,21 @@
+package exercise;
+
+public class CheckOverlap {
+    public static boolean checkOverlap(int radius, int x_center, int y_center, int x1, int y1, int x2, int y2) {
+        int dx=0, dy=0;
+        if((x1-x_center) * (x2-x_center) > 0) {
+        	dx = Math.min(Math.abs(x1-x_center), Math.abs(x2-x_center));
+        }
+        if((y1-y_center) * (y2-y_center) > 0) {
+        	dy = Math.min(Math.abs(y1-y_center), Math.abs(y2-y_center));
+        }
+        
+        return dx*dx + dy*dy <= radius*radius;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
