43bed5bb02b577e7b1f9708e99d551b402b41ab9
jipram019 <jipram019@gmail.com>
2020-07-03 13:01:45 +0700

    linked list intersection

---
+++ src/exercises/LinkedListIntersection.java
@@ -0,0 +1,24 @@
+package exercises;
+
+import exercises.Node.ListNode;
+
+public class LinkedListIntersection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if(headA == null || headB == null) return null;
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while(pA != pB) {
+          	pA = pA != null? pA.next: headB;
+        	pB = pB != null? pB.next: headA;
+        }
+        
+        return (pA==pB && pA != null)? pA:null;
+    }
+
+}
