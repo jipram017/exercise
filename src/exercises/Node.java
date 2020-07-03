9c81396d9120d2c28e6c699d742b9bb1f0eec086
jipram019 <jipram019@gmail.com>
2020-07-03 13:02:27 +0700

    added node definition

---
+++ src/exercises/Node.java
@@ -0,0 +1,13 @@
+package exercises;
+
+public class Node {
+	 // Definition for singly-linked list.
+	public static class ListNode {
+		 public int val;
+	     public ListNode next;
+	     public ListNode(int x) {
+	    	 val = x;
+	         next = null;
+	     }
+	}
+}
