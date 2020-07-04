e1cf1fbd5d6f5bda9924bfe5dd826426420121d4
jipram019 <jipram019@gmail.com>
2020-07-04 13:24:24 +0700

    perfect square

---
+++ src/exercises/PerfectSquare.java
@@ -0,0 +1,31 @@
+package exercises;
+
+import java.util.Arrays;
+
+public class PerfectSquare {
+	
+	// Time complexity O(n^3/2)
+	// Space complexity O(n)
+    public static int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        int squareRoot = (int) Math.sqrt(n);
+        
+        for(int i = 1; i <= n; i++) {
+        	for(int j = 1; j <= squareRoot; j++) {
+        		if(i >= j*j) {
+        			dp[i] = Math.min(dp[i], 1 + dp[i-j*j]);
+        		}
+        	}
+        }
+        
+        return dp[n];
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
