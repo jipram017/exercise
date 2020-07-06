7339b8dfcc2991ccc21d7e283f45a0b29ba1308d
jipram019 <jipram019@gmail.com>
2020-07-07 02:25:08 +0700

    merge k sorted lists

---
+++ src/exercises/MergeKSortedLists.java
@@ -0,0 +1,40 @@
+package exercises;
+
+import java.util.Comparator;
+import helper.DataStructure.ListNode;
+import java.util.PriorityQueue;
+
+public class MergeKSortedLists {
+    public static ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+        	public int compare(ListNode l1, ListNode l2) {
+        		return l1.val - l2.val;
+        	}
+        });
+        
+        for(ListNode l : lists) {
+        	if(l != null) queue.offer(l);
+        }
+        
+        ListNode p = new ListNode(-1);
+        ListNode head  = p;
+        
+        while(!queue.isEmpty()) {
+        	ListNode top = queue.poll();
+        	head.next = top;
+        	head = top;
+        	if(top.next != null) {
+        		queue.offer(top.next);
+        	}
+        }
+        
+        return p.next;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
