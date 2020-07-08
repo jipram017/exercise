212adac089e07c3d2b36707609c0a105117cc494
jipram019 <jipram019@gmail.com>
2020-07-09 06:26:45 +0700

    gas station

---
+++ src/exercises/GasStation.java
@@ -0,0 +1,24 @@
+package exercises;
+
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+    	int availableGas = 0, neededGas = 0, start = 0;
+    	
+    	for(int i=0; i < gas.length; i++) {
+    		availableGas += gas[i] - cost[i];
+    		if(availableGas < 0) {
+    			start = i+1; availableGas = 0; neededGas += availableGas;
+    		}
+    	}
+    	
+    	if(availableGas + neededGas >= 0) {
+    		return start;
+    	}
+    	return -1;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
