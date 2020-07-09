e60d53ff21e8ff0c503b08a0a754d914f31f1f5e
jipram019 <jipram019@gmail.com>
2020-07-10 03:33:36 +0700

    find duplicate

---
+++ src/exercises/FindDuplicate.java
@@ -0,0 +1,28 @@
+package exercises;
+
+public class FindDuplicate {
+	
+	// Time complexity O(N), space complexity O(1)
+    public int findDuplicate(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        
+        do {
+        	slow = nums[slow];
+        	fast = nums[nums[fast]];
+        } while (slow != fast);
+        
+        int found = 0;
+        while(found != slow) {
+        	found = nums[found];
+        	slow = nums[slow];
+        }
+        
+        return found;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
