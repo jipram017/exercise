53d894eb7eb000838563f21be82c4995f057ea53
jipram019 <jipram019@gmail.com>
2020-07-09 17:47:20 +0700

    number islands

---
+++ src/exercises/NumberIslands.java
@@ -0,0 +1,47 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberIslands {
+	
+	public static List<Integer> numIslands(int[][] matrix) {
+		List<Integer> islandSizes = new ArrayList<Integer>();
+		
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j <matrix[0].length; j++) {
+				if(matrix[i][j] == 1) {
+					int islandSize = traverseIsland(matrix, i, j);
+					islandSizes.add(islandSize);
+				}
+			}
+		}
+		return islandSizes;
+	}
+	
+	public static int traverseIsland(int[][]matrix, int i, int j) {
+		if(i < 0 || i >= matrix.length || j < 0 || j >= matrix[i].length || matrix[i][j] == 0) {
+			return 0;
+		}
+		
+		matrix[i][j] = 0;
+		return 1 + traverseIsland(matrix, i-1, j)
+		         + traverseIsland(matrix, i+1, j)
+		         + traverseIsland(matrix, i, j-1)
+		         + traverseIsland(matrix, i, j+1);
+	}
+
+	public static void main(String[] args) {
+		int M[][] = new int[][] { 
+			{ 1, 1, 0, 0, 0 }, 
+            { 0, 1, 0, 0, 1 }, 
+            { 1, 0, 0, 1, 1 }, 
+            { 0, 0, 0, 0, 0 }, 
+            { 1, 0, 1, 0, 1 } }; 
+        
+            for(Integer n : numIslands(M)) {
+            	 System.out.println(n);
+            }
+	}
+
+}
