d98a1bc803085831f8567001e59780130f602f28
a-pramono <t-aji.pramono@paypay-corp.co.jp>
2022-12-11 18:06:12 +0900

    upd

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ const octokit = github.getOctokit(GITHUB_TOKEN);
 const changelogFilename = 'CHANGELOG.md';
 const changelogAddMessage = 'Added CHANGELOG.md file';
 const changelogUpdateMessage = 'Updated CHANGELOG.md file';
+const init_changelog = core.getInput("init_changelog");
+console.log(init_changelog)
 
 async function run() {
-    const init_changelog = core.getInput("init_changelog");
     if (init_changelog) {await newChangelog();}
     else {await updateChangelog();}
     await createReleaseTag();
