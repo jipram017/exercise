eaccddadf36df485e42883688b666a5ae158432a
a-pramono <t-aji.pramono@paypay-corp.co.jp>
2022-12-11 11:54:23 +0900

    update2

--- dist/index.js
+++ dist/index.js
@@ -9682,40 +9682,83 @@ module.exports = JSON.parse('[[[0,44],"disallowed_STD3_valid"],[[45,46],"valid"]
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
 (() => {
+"use strict";
+
+
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
+const fs = __nccwpck_require__(7147);
+const date = new Date().toISOString().slice(0, 10);
+const version = process.env.npm_package_version;
+const changelogFilename = 'CHANGELOG.md';
 
 async function run() {
-    const TAG_NAME = core.getInput("tag_name");
+    const init_changelog = core.getInput("init_changelog");
+    if (init_changelog) {newChangelog();}
+    else {updateChangelog();}
+    await createReleaseTag();
+}
+
+async function createReleaseTag(){
     const {GITHUB_TOKEN, GITHUB_SHA} = process.env;
     const octokit = github.getOctokit(GITHUB_TOKEN);
     const { context = {} } = github;
-
-    if (!(GITHUB_SHA || GITHUB_TOKEN || TAG_NAME)) {
-        core.setFailed("Missing GITHUB_SHA/GITHUB_TOKEN/TAG_NAME");
-        return;
-    }
+    const TAG_NAME = core.getInput("tag_name");
 
     let ref;
     try {
-        ref = await octokit.rest.git.getRef(
-            {...context.repo, ref: `tags/${TAG_NAME}`}
-        );
+        ref = await octokit.rest.git.getRef({...context.repo, ref: `tags/${TAG_NAME}`});
     } catch (e) {
         if (e.status === 404) {}
         else { throw e;}
     }
     if (!ref) {
-        await octokit.rest.git.createRef(
-            {...context.repo, ref: `refs/tags/${TAG_NAME}`, sha: GITHUB_SHA}
-        );
-    } else {
-        await octokit.rest.git.updateRef(
-            {...context.repo, ref: `tags/${TAG_NAME}`, sha: GITHUB_SHA}
-        );
+        await octokit.rest.git.createRef({...context.repo, ref: `refs/tags/${TAG_NAME}`, sha: GITHUB_SHA});
     }
+    else {
+        await octokit.rest.git.updateRef({...context.repo, ref: `tags/${TAG_NAME}`, sha: GITHUB_SHA});
+    }
+}
+
+function newChangelog() {
+    let changelog = fs.readFileSync(__nccwpck_require__.ab + "init.md", {encoding: 'utf8'});
+    const { version, repository } = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8' }));
+    changelog = changelog.replace('[Unreleased]:', `[Unreleased]: ${getRepositoryUrl(repository, version)}`);
+    fs.writeFileSync(changelogFilename, changelog, {encoding: 'utf8', flag: 'wx'});
+}
+
+function getRepositoryUrl(repository, version) {
+    const getGithubUrl = (name) => `https://github.com/${name}/compare/v${version}...HEAD`;
+    let type;
+    let name;
+    [, type, name] = repository.url.match(/^(?:[\w+]+?:\/\/)?(.+)(?:\/|:)([\w-]+\/[\w-]+)(?:\.git)?$/);
+    return getGithubUrl(name);
+}
+
+function updateChangelog() {
+    let changelog = fs.readFileSync(changelogFilename, { encoding: 'utf8' });
+    changelog = updateUpperSection(changelog);
+    changelog = updateBottomSectionGithub(changelog);
+    fs.writeFileSync(changelogFilename, changelog, { encoding: 'utf8' });
+}
+
+function updateUpperSection(changelog) {
+    return changelog.replace(/## \[Unreleased\]/, `## [Unreleased]\n\n## [${version}] - ${date}`);
+}
+
+function updateBottomSectionGithub(changelog) {
+    const regex = /\[Unreleased\]:(.+\/)v(.+)\.\.\.HEAD/i;
+    const matches = changelog.match(regex);
+    if (matches) {
+        const url = matches[1];
+        const previousVersion = matches[2];
+        const compareToUnreleased = `[Unreleased]:${url}v${version}...HEAD`;
+        const compareToLatestVersion = `[${version}]:${url}v${previousVersion}...v${version}`;
+        changelog = changelog.replace(regex, `${compareToUnreleased}\n${compareToLatestVersion}`);
+    }
+    return changelog;
 }
 
 run();
